fd285cec3f89ca07459fcede2b63b1177fa1b8d6
deve1070 <dkassa253@gmail.com>
2025-08-19 22:03:12 +0300

    refactored check events function, completed the events related to ship's movement

--- ship.py
+++ ship.py
@@ -13,10 +13,30 @@ class Ship:
         # Load the ship image and get its rect.
         self.image=pygame.image.load('images/ship.bmp')
         self.rect=self.image.get_rect()
+        # Movement flags
+        self.moving_left=False
+        self.moving_right=False
+        self.settings=ai_game.settings
 
-
-        # Start each new ship at the bottom center of the screen.
+          # Start each new ship at the bottom center of the screen.
         self.rect.midbottom=self.screen_rect.midbottom
+    
+
+        # Store a decimal value for the ship's horizontal position
+        self.x=float(self.rect.x)
+
+      
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x +=self.settings.ship_speed
+        if self.moving_left and self.rect.left >0:
+            self.x -=self.settings.ship_speed
+        
+        # Update rect object from self.x
+        self.rect.x=self.x
+
+
 
 
     def blitme(self):
