d3289b079b466d113377cc3d3d9cee8376ddfadf
deve1070 <dkassa253@gmail.com>
2025-08-23 23:42:28 +0300

    added helper function for updating bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,10 +28,10 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
+                    
            
-            # Redraw the screen during each pass through the loop.
           
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -66,6 +66,18 @@ class AlienInvasion:
         """Create a new bullet and add it to the bullets group."""
         new_bullet=Bullet(self)
         self.bullets.add(new_bullet)
+    
+    def _update_bullets(self):
+        """"Update position of bullets and get rid of old bullets"""
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <=0:
+                self.bullets.remove(bullet)
+
+        
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
         self.screen.fill(self.bg_color)
